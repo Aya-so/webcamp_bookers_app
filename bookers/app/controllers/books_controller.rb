80a85c42cddcecfcb66c9aacee1efa6351b2922d
Aya-so <ayaka.wakame0315@gmail.com>
2021-05-12 11:10:26 +0000

    [Addバリデーション追加

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,14 +1,21 @@
 class BooksController < ApplicationController
 
+  def new
+    @book = Book.new
+  end
+
   def index
     @books = Book.all
-    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+
+    if book.save
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
 
     flash[:notice] = "Book was successfully created."
   end
